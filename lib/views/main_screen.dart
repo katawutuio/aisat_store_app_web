bb5870b35253c52a98b84b4eb42bbe62e5320617
Katawut Keng <katawutu.io@gmail.com>
2024-10-06 01:47:09 +0700

    create sidebar - add sideBar เพื่อสร้าง sidebar menu - add AdminMenuItem - ใช้ CupertinoIcons package เพื่อเอา icon มาแสดง หรรือใช้ Icons

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_admin_scaffold/admin_scaffold.dart';
 
@@ -14,10 +15,35 @@ class _MainScreenState extends State<MainScreen> {
     return AdminScaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Text('Management'),
+        title: const Text('Management'),
       ),
-      body: Text(
+      body: const Text(
         'Dashboard',
+      ),
+      sideBar: const SideBar(
+        items: [
+          AdminMenuItem(
+            title: 'Vendors',
+            route: '',
+            icon: CupertinoIcons.person_3,
+          ),
+          AdminMenuItem(
+            title: 'Buyers',
+            route: '',
+            icon: CupertinoIcons.person,
+          ),
+          AdminMenuItem(
+            title: 'Categories',
+            route: '',
+            icon: Icons.category,
+          ),
+          AdminMenuItem(
+            title: 'Banners',
+            route: '',
+            icon: CupertinoIcons.bandage,
+          ),
+        ],
+        selectedRoute: '',
       ),
     );
   }
