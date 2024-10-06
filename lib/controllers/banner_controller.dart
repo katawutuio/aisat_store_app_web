670b01799a7437da1d6711897842dc5bf8f3ab08
Katawut Keng <katawutu.io@gmail.com>
2024-10-06 18:16:52 +0700

    - create loadBanners function โดยใช้ Futer<List<BannerModel>> - รับ response แล้วแปลงมาใส่ List dynamic: List<dynamic> data = jsonDecode(resonse.body); - map data from json to banners variable - throw error  ถ้าโหลดข้อมูลไม่ได้

--- lib/controllers/banner_controller.dart
+++ lib/controllers/banner_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:aisat_store_app_web/global_variable.dart';
 import 'package:aisat_store_app_web/models/banner.dart';
 import 'package:aisat_store_app_web/services/manage_http_response.dart';
@@ -32,6 +34,32 @@ class BannerController {
           });
     } catch (e) {
       print(e);
+    }
+  }
+
+  // fetch banners
+  Future<List<BannerModel>> loadBanners() async {
+    try {
+      // send an http get request to fetch banners
+      http.Response response = await http.get(
+        Uri.parse('$uri/api/banner'),
+        headers: <String, String>{
+          "Content-Type": 'application/json; charset=UTF-8'
+        },
+      );
+
+      print(response.body);
+      if (response.statusCode == 200) {
+        List<dynamic> data = jsonDecode(response.body);
+        List<BannerModel> banners =
+            data.map((banner) => BannerModel.fromJson(banner)).toList();
+        return banners;
+      } else {
+        // throw an exception
+        throw Exception('Failed to load banners');
+      }
+    } catch (e) {
+      throw Exception('Error loading Banners $e');
     }
   }
 }
