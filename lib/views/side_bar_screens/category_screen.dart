f0d9fc2d9ee4e8aed11ed00d259f4c6327a89d3a
Katawut Keng <katawutu.io@gmail.com>
2024-10-06 15:44:06 +0700

    Create category controller - dynamic type = data type เป็นอะไรก็ได้ - ใช้ CloudinaryPubic(cloudname, uploadPreset) params เอาจาก cloudinary dashboard, uploadPreset เอาจากตอน getAPiKey -> uploadPreset -> App uplod preset - เลือก signing mode เป็น unsigned - allow user เข้าใาดูรูปของเราได้ - enable unsign uploading - copy upload preset name --- - crate upload image function ด้วย cloudinary.uploadFile และเก็บ response ไว้ใน CloudinaryResponse - เพิ่ม category controller เข้ามาใน category_screen และเรียกใช้ upload function - หลัง uplod แล้วเช็ครูปได้ที่ cloudinary asset

--- lib/views/side_bar_screens/category_screen.dart
+++ lib/views/side_bar_screens/category_screen.dart
@@ -1,3 +1,4 @@
+import 'package:aisat_store_app_web/controllers/category_controller.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,7 @@ class CategoryScreen extends StatefulWidget {
 
 class _CategoryScreenState extends State<CategoryScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final CategoryController categoryController = CategoryController();
   late String categoryName;
   dynamic _image;
   dynamic _bannerImage;
@@ -113,9 +115,10 @@ class _CategoryScreenState extends State<CategoryScreen> {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.blue,
                 ),
-                onPressed: () {
+                onPressed: () async {
                   if (_formKey.currentState!.validate()) {
-                    print(categoryName);
+                    categoryController.uploadCategory(
+                        pickedImage: _image, pickedBanner: _bannerImage);
                   }
                 },
                 child: const Text(
