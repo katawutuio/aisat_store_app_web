30af3a6aba1b95580297468e362f9f902d1231ee
Katawut Keng <katawutu.io@gmail.com>
2024-10-06 12:29:52 +0700

    - สร้าง FormKey เพื่อทำ validate - set key ที่ Form Widget - ใช้ final variable เพราะจะไม่มีการเปลี่ยนแปลงค่าของ variable - Wrap Column widget ด้วย Form widget เพื่อทำ validation ได้ - สร้าง late variable เพื่อมาเก็บชื่อ category - ใส่ inChanged ที่ textformfield เพื่อทำการเอาค่าที่ user พิมไปใส่ late variable

--- lib/views/side_bar_screens/category_screen.dart
+++ lib/views/side_bar_screens/category_screen.dart
@@ -10,6 +10,8 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late String categoryName;
   dynamic _image;
 
   pickImage() async {
@@ -27,88 +29,105 @@ class _CategoryScreenState extends State<CategoryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            alignment: Alignment.topLeft,
-            child: Text(
-              'Category',
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Divider(
-            color: Colors.grey,
-          ),
-        ),
-        Row(
-          children: [
-            Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(
-                  5,
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Category',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              child: Center(
-                child: _image != null
-                    ? Image.memory(_image)
-                    : Text('Category image'),
-              ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: 200,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Enter Category Name',
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Divider(
+              color: Colors.grey,
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(
+                    5,
+                  ),
+                ),
+                child: Center(
+                  child: _image != null
+                      ? Image.memory(_image)
+                      : Text('Category image'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 200,
+                  child: TextFormField(
+                    onChanged: (value) {
+                      categoryName = value;
+                    },
+                    validator: (value) {
+                      if (value!.isNotEmpty) {
+                        return null;
+                      } else {
+                        return 'Please enter category name';
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Enter Category Name',
+                    ),
                   ),
                 ),
               ),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('cancel'),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
+              TextButton(
+                onPressed: () {},
+                child: Text('cancel'),
               ),
-              onPressed: () {},
-              child: Text(
-                'save',
-                style: TextStyle(color: Colors.white),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    print(categoryName);
+                  }
+                },
+                child: Text(
+                  'save',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                pickImage();
+              },
+              child: Text('Pick image'),
             ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton(
-            onPressed: () {
-              pickImage();
-            },
-            child: Text('Pick image'),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Divider(
-            color: Colors.grey,
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Divider(
+              color: Colors.grey,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
