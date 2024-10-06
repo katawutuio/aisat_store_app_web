51088c1a7f462f971693751eac9e5b70638a4c7f
Katawut Keng <katawutu.io@gmail.com>
2024-10-06 14:54:41 +0700

    create http service สำหรับ manage respose (ใช้ของ ตัว app มาได้เลย)

---
+++ lib/services/manage_http_response.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void manageHttpResponse({
+  required http.Response response, // the HTTP response from the request
+  required BuildContext context, // the context is to show snackbar
+  required VoidCallback
+      onSuccess, // the callback to execute on a successful response
+}) {
+  // switch statement to handle different http status codes
+  switch (response.statusCode) {
+    case 200:
+      onSuccess();
+      break;
+    case 400:
+      showSnackBar(context, json.decode(response.body)['msg']);
+      break;
+    case 500:
+      showSnackBar(context, json.decode(response.body)['error']);
+      break;
+    case 201:
+      onSuccess();
+      break;
+  }
+}
+
+void showSnackBar(BuildContext context, String title) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(title)));
+}
